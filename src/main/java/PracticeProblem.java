5c77412ba59648ec40649f519830219b628fc4f6
roy-edwin-ics4u1-03-25 <roy.edwin@student.tdsb.on.ca>
2025-04-08 05:34:34 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,29 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static int sum2D(int[][] int2d){
+		int x=0;
+		for(int i = 0; i < int2d.length;i++){
+			for(int z=0;z<int2d[i].length;z++){
+				x += int2d[i][j];
+			}
+		}
+		return x;
+	}
 
+	public static int sumRow(int[][] int2d, int o){
+		int x=0;
+		for(int i=0;i<int2d[o].length;i++){
+			x += int2d[o][i];
+		}
+		return x;
+	}
+
+	public static int sumColumn(int[][] int2d, int o){
+		int x=0;
+		for(int i=0;i<int2d.length;i++){
+			x += int2d[i][o];
+		}
+		return x;
+	}
 }
